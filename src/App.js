f059764e92d237a988a75cd89201f93194c3ea8c
joelcmk <57118300+joelcmk@users.noreply.github.com>
2021-10-03 17:09:31 -0500

    added state and use effect function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [name, setName] = useState('');
+  const [state, setState] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name)
+  }
+
+  useEffect(() => {
+    fetch('https://api.agify.io/?name=')
+      .then(response => response.json())
+      .then(data => {
+        setState(data)
+      })
+      .catch(err => 'something went wrong')
+  }, [])
+
+  console.log(state)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text" id="name"
+          name="name"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <h2>{}</h2>
     </div>
   );
 }
